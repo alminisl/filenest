e777cbfcde27e69ed103ec5905026884f4e6e617
Almin Islamovic <almin.islamovic@gmail.com>
2023-06-06 10:20:04 +0200

    Fixed some pesky CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import logo from "../icon/icon.png";
 
 interface Folders {
   name: string;
@@ -46,76 +47,86 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <div className="input-container">
-            <label htmlFor="base-path-input">Base path</label>
-            <input
-              id="base-path-input"
-              onChange={(e) => setBasePath(e.currentTarget.value)}
-              value={basePath}
-              disabled={true}
-            />
-          </div>
-
-          {folders.map((folder) => (
-            <div key={folder.name} className="input-container">
-              <label htmlFor="folder-input" className="folder-label">
-                {folder.name}
-              </label>
-              <div className="input-background">
-                <input
-                  id="folder-input"
-                  className="folder-input"
-                  onChange={(e) => {
-                    const newFolders = folders.map((f) => {
-                      if (f.name === folder.name) {
-                        return {
-                          ...f,
-                          path: e.currentTarget.value,
-                        };
-                      }
-                      return f;
-                    });
-                    setFolders(newFolders);
-                  }}
-                  value={folder.path}
-                />
-                <label htmlFor="folder-extension" className="folder-label">
-                  Extension:
-                </label>
-                <input
-                  id="folder-extensions"
-                  className="folder-extensions"
-                  value={folder.extensions.join(",")}
-                  onChange={(e) => {
-                    const newFolders = folders.map((f) => {
-                      if (f.name === folder.name) {
-                        return {
-                          ...f,
-                          extensions: e.currentTarget.value.split(","),
-                        };
-                      }
-                      return f;
-                    });
-                    setFolders(newFolders);
-                  }}
-                />
-              </div>
-            </div>
-          ))}
-          <button onClick={(e) => updateConfig()} className="save-button">
-            Save
-          </button>
-          <button onClick={(e) => sortItems()}>Re-sort</button>
-        </form>
+    <>
+      <div className="logo">
+        <img src={logo} alt="Logo" className="logo-image" />
+        <label className="logo-label">Filenest</label>
       </div>
-    </div>
+
+      <div className="container">
+        <div className="row">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="input-container">
+              <label htmlFor="base-path-input">Base path</label>
+              <input
+                id="base-path-input"
+                onChange={(e) => setBasePath(e.currentTarget.value)}
+                value={basePath}
+                disabled={true}
+              />
+            </div>
+
+            {folders.map((folder) => (
+              <div key={folder.name} className="input-container">
+                <label htmlFor="folder-input" className="folder-label">
+                  {folder.name}
+                </label>
+                <div className="input-background">
+                  <input
+                    id="folder-input"
+                    className="folder-input"
+                    onChange={(e) => {
+                      const newFolders = folders.map((f) => {
+                        if (f.name === folder.name) {
+                          return {
+                            ...f,
+                            path: e.currentTarget.value,
+                          };
+                        }
+                        return f;
+                      });
+                      setFolders(newFolders);
+                    }}
+                    value={folder.path}
+                  />
+                  <label htmlFor="folder-extension" className="folder-label">
+                    Extension:
+                  </label>
+                  <input
+                    id="folder-extensions"
+                    className="folder-extensions"
+                    value={folder.extensions.join(",")}
+                    onChange={(e) => {
+                      const newFolders = folders.map((f) => {
+                        if (f.name === folder.name) {
+                          return {
+                            ...f,
+                            extensions: e.currentTarget.value.split(","),
+                          };
+                        }
+                        return f;
+                      });
+                      setFolders(newFolders);
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+            <div>
+              <button>PLACEHOLDER: ADD NEW FOLDER</button>
+            </div>
+            <button onClick={(e) => updateConfig()} className="save-button">
+              Save
+            </button>
+            <button onClick={(e) => sortItems()}>Re-sort</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
